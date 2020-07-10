334c87f7b478f89dc7d8c55ea85a1b6b89a2e9cd
Natalie Michelsen <natalie.michelsen@outlook.com>
2020-07-10 15:39:21 -0600

    html-routes.js final updates

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,14 +1,21 @@
-// require
+// require - ✓
 const express = require('express');
 const router = express.Router();
-const path = require("path");
 
+// routers - ✓
+// fixed res.sendFile(code) for mongo
 router.get("/exercise", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  res.sendFile(`$({process.cwd()}/../public/exercise.html)`);
 });
 
 router.get("/stats", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/stats.html"));
+  res.sendFile(`$({process.cwd()}/../public/stats.html)`);
 });
 
+// export - ✓
 module.exports = router;
+
+// needed to construct this page from existing code
+  // require at top was slightly off
+    // routers were mostly alright with a few changes
+      // exports was good
--- server.js
+++ server.js
@@ -1,29 +1,31 @@
-// require
+// require - ✓
 const express = require('express');
 const mongoose = require('mongoose');
 
-// server connection
+// server connection - ✓
 const server = express();
 
-// port
+// port = ✓
 const PORT = process.env.PORT || 3000;
 
-// mongoose connection
+// mongoose connection - ✓
 mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://localhost/workout',
     { useNewUrlParser: true }
 );
 
-// express servers
+// express servers - ✓
 server.use(express.static('public'));
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
-// routes
+// routes - ✓
 server.use(require('./routes/api-routes'));
 server.use(require('./routes/html-routes'));
 
-// listen
+// listen - ✓
 server.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`)
 });
+
+// needed to construct this page from scratch
