766e9e375de372148dd48e0de8a1d601c81e8dac
Natalie Michelsen <natalie.michelsen@outlook.com>
2020-07-10 16:44:47 -0600

    api-routes updates 2

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,49 +1,51 @@
-// require - ✓ (match html)
+// require - ✓ (matched to  html)
 // 'workout' not necessary
 const express = require('express');
 const router = express.Router();
 
-// require db 
+// require db added - ✓
 const db = require('../models')
 
-// 
-router.post("/api/workouts", (req, res) => {
-  Workout.create({})
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
+// get - ✓
+// moved to top
+// needed some code additions / adjustments for proper connection / functionality
+router.get("/api/workouts", (req, res) => {
+  db.Workout.find().then(dbExercise => {
+    res.json(dbExercise);
+  })
     .catch(err => {
       res.json(err);
     });
 });
 
-// 
-router.put("/api/workouts/:id", ({ body, params }, res) => {
-  Workout.findByIdAndUpdate(
+// post - ✓
+// needed some code additions / adjustments for proper connection / functionality
+router.post("/api/workouts", ({ body }, res) => {
+  db.Workout.create(body).then(dbExercise => {
+    res.json(dbExercise);
+  })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+// put - ✓
+// needed some code additions / adjustments for proper connection / functionality
+router.put("/api/workouts/:id", (req, res) => {
+  db.Workout.findByIdAndUpdate(
     params.id,
     { $push: { exercises: body } },
     // "runValidators" will ensure new exercises meet our schema requirements
     { new: true, runValidators: true }
-  )
-    .then(dbWorkout => {
-      res.json(dbWorkout);
+  ).then(dbExercise => {
+      res.json(dbExercise);
     })
     .catch(err => {
       res.json(err);
     });
 });
 
-// 
-router.get("/api/workouts", (req, res) => {
-  Workout.find()
-    .then(dbWorkouts => {
-      res.json(dbWorkouts);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
+// get - ✓
 // 
 router.get("/api/workouts/range", (req, res) => {
   Workout.find({}).limit(7)
@@ -56,16 +58,12 @@ router.get("/api/workouts/range", (req, res) => {
     });
 });
 
-// 
-router.delete("/api/workouts", ({ body }, res) => {
-  Workout.findByIdAndDelete(body.id)
-    .then(() => {
-      res.json(true);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
+// delete unnecessary -removed-
 
-// 
+// export - ✓
 module.exports = router;
+
+// needed to construct this page from existing code
+  // require at top was slightly off - matched it to html-routes
+    // routers were mostly alright with a few changes and additions
+      // exports was good
