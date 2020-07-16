0c91680728965c738444bbbf65f5f76ae49a10c4
Natalie Michelsen <natalie.michelsen@outlook.com>
2020-07-16 14:53:27 -0600

    pages are linked now

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,15 +2,16 @@
 // path not necessary
 const express = require('express');
 const router = express.Router();
+const path = require('path')
 
 // routers - ✓
 // fixed res.sendFile(code) for mongo
 router.get("/exercise", (req, res) => {
-  res.sendFile(`$({process.cwd()}../public/exercise.html`);
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
 router.get("/stats", (req, res) => {
-  res.sendFile(`$({process.cwd()}../public/stats.html`);
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 
 // export - ✓
